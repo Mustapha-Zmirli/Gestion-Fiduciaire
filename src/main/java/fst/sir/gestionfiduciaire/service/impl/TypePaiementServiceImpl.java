01c82ceb4c0681b0b206f14941ec426ef24e2a5e
Mustapha-Zmirli <mostafazmirli@gmail.com>
2024-03-09 16:45:41 +0100

    Paiement Developed successfully

---
+++ src/main/java/fst/sir/gestionfiduciaire/service/impl/TypePaiementServiceImpl.java
@@ -0,0 +1,40 @@
+package fst.sir.gestionfiduciaire.service.impl;
+
+import fst.sir.gestionfiduciaire.bean.paiement.TypePaiement;
+import fst.sir.gestionfiduciaire.dao.TypePaiementDao;
+import fst.sir.gestionfiduciaire.service.facade.TypePaiementService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TypePaiementServiceImpl implements TypePaiementService {
+
+
+    public TypePaiementServiceImpl(TypePaiementDao typePaiementDao) {
+        this.typePaiementDao = typePaiementDao;
+    }
+
+    @Override
+    public TypePaiement findByCode(String code) {
+        return typePaiementDao.findByCode(code);
+    }
+    @Transactional
+    @Override
+    public int deleteByCode(String code) {
+        return typePaiementDao.deleteByCode(code);
+    }
+
+    @Override
+    public int save(TypePaiement typePaiement) {
+        if (findByCode(typePaiement.getCode()) == null) {
+            return -1;
+        } else {
+            typePaiementDao.save(typePaiement);
+        }
+        return 1;
+    }
+
+    private final TypePaiementDao typePaiementDao;
+
+
+}
